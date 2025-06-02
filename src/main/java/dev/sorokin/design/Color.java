dd5f9ed5b5f7164b878b4e3ca3c0cd190d88874d
Novichek290 <dw290417@gmail.com>
2025-06-02 10:58:41 +0300

    добавил чтобы можно было выделять и разделять сообщения по цвету

---
+++ src/main/java/dev/sorokin/design/Color.java
@@ -0,0 +1,16 @@
+package dev.sorokin.design;
+
+import lombok.Getter;
+
+@Getter
+public class Color {
+    private final String RESET = "\u001B[0m";
+    private final String BLACK = "\u001B[30m";
+    private final String RED = "\u001B[31m";
+    private final String GREEN = "\u001B[32m";
+    private final String YELLOW = "\u001B[33m";
+    private final String BLUE = "\u001B[34m";
+    private final String PURPLE = "\u001B[35m";
+    private final String CYAN = "\u001B[36m";
+    private final String WHITE = "\u001B[37m";
+}
