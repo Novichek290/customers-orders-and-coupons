43ee6ca5cb39ca64971e1ccc183bd59a578f5f8b
Novichek290 <dw290417@gmail.com>
2025-06-10 10:29:40 +0300

    Уяснил, что для корректного присвоения профиля клиенту нужно profile.setClient(client);  client.setProfile(profile);, а после уже персистеть клиента.
     В остальном только лишний код написал, который пытался решить то, что и так работает

--- src/main/java/dev/sorokin/Main.java
+++ src/main/java/dev/sorokin/Main.java
@@ -1,31 +1,49 @@
 package dev.sorokin;
 
 import dev.sorokin.configuration.HibernateConfiguration;
+import dev.sorokin.design.Color;
 import dev.sorokin.entity.Client;
 import dev.sorokin.entity.Profile;
 import dev.sorokin.service.ClientService;
 import dev.sorokin.service.ProfileService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.time.LocalDateTime;
+
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		try (AnnotationConfigApplicationContext context =
-					 new AnnotationConfigApplicationContext(HibernateConfiguration.class)) {
-				ClientService clientService = context.getBean(ClientService.class);
-				ProfileService profileService = context.getBean(ProfileService.class);
+        try (AnnotationConfigApplicationContext context =
+                     new AnnotationConfigApplicationContext(HibernateConfiguration.class)) {
+            ClientService clientService = context.getBean(ClientService.class);
+            ProfileService profileService = context.getBean(ProfileService.class);
 
-			Client client = clientService.getById(44L);
-			Profile profile = profileService.profileGet(2L);
+            Client client = Client.builder()
+                    .name("Adam")
+                    .email("IFuckEvaToo@in.ass")
+                    .dateTime(LocalDateTime.now())
+                    .build();
 
-//			profileService.profileDelete(2L);
+            Profile profile = Profile.builder()
+                    .address("Gods heaven, 1")
+                    .phone("not-your-business")
+                    .build();
 
-			client.setProfile(profile);
-			profile.setClient(client);
+            profile.setClient(client);
+            client.setProfile(profile);
 
-			clientService.update(client);
-			clientService.printAll();
-			}
-		}
+//            clientService.printAll();
+            System.out.print(Color.getBLUE() + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+            System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + Color.getRESET());
+            clientService.create(client);
+            System.out.print(Color.getBLUE() + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+            System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + Color.getRESET());
+
+//            for(Client mrX : clientService.getAll()) {
+//                clientService.delete(mrX.getId());
+//            }
+            clientService.printAll();
+        }
+    }
 }
