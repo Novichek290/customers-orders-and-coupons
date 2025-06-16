8bcb13ac6f41cdb14ff94e8c4f056935fac28690
Novichek290 <dw290417@gmail.com>
2025-06-16 09:03:52 +0300

    ничего существенного. Тут я обкатываю разное, перед добавлением в класс или метод.

--- src/main/java/dev/sorokin/Main.java
+++ src/main/java/dev/sorokin/Main.java
@@ -3,12 +3,17 @@ package dev.sorokin;
 import dev.sorokin.configuration.HibernateConfiguration;
 import dev.sorokin.design.Color;
 import dev.sorokin.entity.Client;
+import dev.sorokin.entity.Order;
 import dev.sorokin.entity.Profile;
 import dev.sorokin.service.ClientService;
+import dev.sorokin.service.OrderService;
+import dev.sorokin.service.OrderStatus;
 import dev.sorokin.service.ProfileService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
@@ -17,33 +22,55 @@ public class Main {
         try (AnnotationConfigApplicationContext context =
                      new AnnotationConfigApplicationContext(HibernateConfiguration.class)) {
             ClientService clientService = context.getBean(ClientService.class);
+            OrderService orderService = context.getBean(OrderService.class);
             ProfileService profileService = context.getBean(ProfileService.class);
 
-            Client client = Client.builder()
-                    .name("Adam")
-                    .email("IFuckEvaToo@in.ass")
-                    .dateTime(LocalDateTime.now())
+            System.out.println(Color.getYELLOW() + "сразу после context" + Color.getRESET());
+
+//            Client client = Client.builder()
+//                    .name("Seller228")
+//                    .email("IFucker228@in.ass")
+//                    .dateTime(LocalDateTime.now())
+//                    .build();
+
+            Client client = ClientService.getById(80L);
+            System.out.println(Color.getYELLOW() + "вернул клиента" + Color.getRESET());
+
+            Order car = Order.builder()
+                    .totalAmount(2)
+                    .orderDate(LocalDateTime.now())
+                    .orderStatus(OrderStatus.PROCESSING)
+                    .client(client)
                     .build();
 
-            Profile profile = Profile.builder()
-                    .address("Gods heaven, 1")
-                    .phone("not-your-business")
-                    .build();
+            System.out.println(Color.getYELLOW() + "создал заказ:" + Color.getRESET());
+            System.out.printf(Color.getCYAN() + "total Amount: %s | orderDate: %-12s | status: " + Color.getPURPLE() + "%-12s\n", car.getTotalAmount(), car.getOrderDate(), car.getOrderStatus());
+            System.out.print(Color.getRESET());
 
-            profile.setClient(client);
-            client.setProfile(profile);
+//            Profile profile = Profile.builder()
+//                    .address("Gods heaven, 1")
+//                    .phone("not-your-business")
+//                    .build();
 
-//            clientService.printAll();
-            System.out.print(Color.getBLUE() + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + Color.getRESET());
-            clientService.create(client);
-            System.out.print(Color.getBLUE() + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-            System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + Color.getRESET());
+            client.addOrder(car);
+            orderService.orderCreate(car);
+
+
+
+            System.out.println(Color.getYELLOW() + "клиенту установил заказ" + Color.getRESET());
+            borders();
+
+            borders();
+
 
 //            for(Client mrX : clientService.getAll()) {
 //                clientService.delete(mrX.getId());
 //            }
             clientService.printAll();
         }
+    }
+    public static void borders() {
+        System.out.print(Color.getBLUE() + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + Color.getRESET());
     }
 }
