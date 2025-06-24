36ed8eac86c2de35d7b5641343d35575dfd3fecc
Novichek290 <dw290417@gmail.com>
2025-06-24 10:09:43 +0300

    Мейн пока полигон. Создал метод очищающий бд и метод notification, выводящий нотификейшн жёлтого цвета. Достаточно удобно и компактно. имеет вид notification("your notification"); Куда приятнее, чем конструкция System.out.println(Color.getYELLOW() + "your notification" + Color.getRESET());, даже если исключить цвет.

--- src/main/java/dev/sorokin/Main.java
+++ src/main/java/dev/sorokin/Main.java
@@ -1,19 +1,18 @@
 package dev.sorokin;
 
 import dev.sorokin.configuration.HibernateConfiguration;
-import dev.sorokin.design.Color;
+import dev.sorokin.controller.Console;
+import dev.sorokin.util.Color;
 import dev.sorokin.entity.Client;
 import dev.sorokin.entity.Order;
-import dev.sorokin.entity.Profile;
 import dev.sorokin.service.ClientService;
 import dev.sorokin.service.OrderService;
-import dev.sorokin.service.OrderStatus;
+import dev.sorokin.util.OrderStatus;
 import dev.sorokin.service.ProfileService;
+import dev.sorokin.util.TransactionHelper;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Main {
@@ -24,17 +23,17 @@ public class Main {
             ClientService clientService = context.getBean(ClientService.class);
             OrderService orderService = context.getBean(OrderService.class);
             ProfileService profileService = context.getBean(ProfileService.class);
+            Console console = context.getBean(Console.class);
 
-            System.out.println(Color.getYELLOW() + "сразу после context" + Color.getRESET());
+            notification("сразу после context");
 
-//            Client client = Client.builder()
-//                    .name("Seller228")
-//                    .email("IFucker228@in.ass")
-//                    .dateTime(LocalDateTime.now())
-//                    .build();
+            Client client = Client.builder()
+                    .name("Seller228")
+                    .email("mail")
+                    .dateTime(LocalDateTime.now())
+                    .build();
 
-            Client client = ClientService.getById(80L);
-            System.out.println(Color.getYELLOW() + "вернул клиента" + Color.getRESET());
+            notification("клиент создан, но не добавлен в БД");
 
             Order car = Order.builder()
                     .totalAmount(2)
@@ -43,34 +42,40 @@ public class Main {
                     .client(client)
                     .build();
 
-            System.out.println(Color.getYELLOW() + "создал заказ:" + Color.getRESET());
-            System.out.printf(Color.getCYAN() + "total Amount: %s | orderDate: %-12s | status: " + Color.getPURPLE() + "%-12s\n", car.getTotalAmount(), car.getOrderDate(), car.getOrderStatus());
+            notification("создан заказ:");
+            System.out.printf(Color.getCYAN() + "total Amount: %s | orderDate: %-12s | status: " + Color.getPURPLE() + "%-12s\n",
+                    car.getTotalAmount(),
+                    car.getOrderDate(),
+                    car.getOrderStatus());
             System.out.print(Color.getRESET());
-
-//            Profile profile = Profile.builder()
-//                    .address("Gods heaven, 1")
-//                    .phone("not-your-business")
-//                    .build();
 
             client.addOrder(car);
             orderService.orderCreate(car);
 
-
-
-            System.out.println(Color.getYELLOW() + "клиенту установил заказ" + Color.getRESET());
-            borders();
+            notification("клиенту установил заказ");
 
             borders();
-
-
-//            for(Client mrX : clientService.getAll()) {
-//                clientService.delete(mrX.getId());
-//            }
-            clientService.printAll();
+            console.printAll();
         }
     }
     public static void borders() {
         System.out.print(Color.getBLUE() + "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
         System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + Color.getRESET());
+    }
+
+    public static void deleteAllFromDB() {
+        try (AnnotationConfigApplicationContext context =
+                     new AnnotationConfigApplicationContext(HibernateConfiguration.class)) {
+            Console console = context.getBean(Console.class);
+            ClientService clientService = context.getBean(ClientService.class);
+
+            for (Client mrX : console.getAll()) {
+                clientService.delete(mrX.getId());
+            }
+        }
+    }
+
+    public static void notification(String notification) {
+        System.out.println(Color.getYELLOW() + notification + Color.getRESET());
     }
 }
