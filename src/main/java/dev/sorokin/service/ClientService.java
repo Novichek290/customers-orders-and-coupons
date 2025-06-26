f74526ba33a50b5d0aa91caba6c125253aae10fb
Novichek290 <dw290417@gmail.com>
2025-06-26 09:55:39 +0300

    -

--- src/main/java/dev/sorokin/service/ClientService.java
+++ src/main/java/dev/sorokin/service/ClientService.java
@@ -1,21 +1,20 @@
 package dev.sorokin.service;
 
-import dev.sorokin.controller.Console;
 import dev.sorokin.util.Color;
 import dev.sorokin.entity.Client;
 import dev.sorokin.entity.Profile;
 import dev.sorokin.exeption.ClientEmailAlreadyExists;
 import dev.sorokin.util.TransactionHelper;
+import lombok.AllArgsConstructor;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class ClientService {
     private final TransactionHelper transactionHelper;
-
-    public ClientService(TransactionHelper transactionHelper) {
-        this.transactionHelper = transactionHelper;
-    }
 
     public Client create(Client client) {
         return transactionHelper.executeInTransaction(session -> {
@@ -50,6 +49,34 @@ public class ClientService {
         });
     }
 
+    public List<Client> getAll() {
+        return transactionHelper.executeInTransaction(session -> {
+            return session.createQuery("SELECT c FROM Client c").getResultList();
+        });
+    }
+
+    public Client getById(Long id) {
+        return transactionHelper.executeInTransaction(session -> {
+            return session.get(Client.class, id);
+        });
+    }
+
+    public List<Client> getByName(String name) {
+        return transactionHelper.executeInTransaction(session -> {
+            return session.createQuery("SELECT c FROM Client c where c.name = :name", Client.class)
+                    .setParameter("name", name).
+                    getResultList();
+        });
+    }
+
+    public List<Client> getByEmail(String email) {
+        return transactionHelper.executeInTransaction(session -> {
+            return session.createQuery("SELECT c FROM Client c where c.email = :email", Client.class)
+                    .setParameter("email", email).
+                    getResultList();
+        });
+    }
+
     public void clientUpdate(Long id, String newName, String newEmail) {
         transactionHelper.executeInTransaction(session -> {
             session.createQuery("UPDATE Client c SET c.name = :name, c.email = :email where c.id = :id")
@@ -62,8 +89,7 @@ public class ClientService {
     }
 
     public Client profileUpdate(Long clientId, String address, String phone) {
-        Console console = new Console(transactionHelper);
-        Client client = console.getById(clientId);
+        Client client = getById(clientId);
         Profile profile = Profile.builder()
                 .phone(phone)
                 .address(address)
